f2a7f5a4fe72ef8e7dd091f534cd6f288c001dc9
Yosif Iliev <yoko.s3m@gmail.com>
2023-11-13 08:32:32 +0200

    Smoother camera tracking

--- src/scene/game/game_camera.lua
+++ src/scene/game/game_camera.lua
@@ -5,9 +5,9 @@ function game_camera:new(o)
     self.__index=self
     
     --init
-    o.offset=-72 --player y offset
-    o.tracking=20 --slowdown player tracking
-    o.track_faster=64 --track faster if player is less than this treshold
+    o.offset=-84 --player y offset
+    o.tracking=26 --slowdown player tracking
+    o.track_faster=74 --track faster if player is less than this treshold
 	o.y = o.y or 0
     o.y+=o.offset
 	return o
@@ -16,7 +16,7 @@ end
 function game_camera:update(player_y)
     local cam_tracking=self.tracking
     --track faster
-    if (player_y-self.y<self.track_faster) cam_tracking/=2.5
+    if (player_y-self.y<self.track_faster) cam_tracking/=2
     --shift
     self.y-=(self.y-(player_y+self.offset))/cam_tracking
 end
